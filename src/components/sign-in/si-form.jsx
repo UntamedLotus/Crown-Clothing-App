2372c0206e7ad2fa1a2d6eaeb8bd81ce59792896
UntamedLotus <ruttikajadhav@gmail.com>
2022-07-23 20:01:46 +0530

    Working with observer

--- src/components/sign-in/si-form.jsx
+++ src/components/sign-in/si-form.jsx
@@ -27,7 +27,7 @@ const SignInForm = () => {
   };
 
   const signInWithGoogle = async () => {
-    const { user } = await signInWithGooglePopup();
+    await signInWithGooglePopup();
   };
 
   const handleSubmit = async (event) => {
