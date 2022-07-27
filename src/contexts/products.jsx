e3a51f3a224820ca3c23c118b9c90ac531b49d6e
UntamedLotus <ruttikajadhav@gmail.com>
2022-07-27 22:32:17 +0530

    Firestore Database setup

--- src/contexts/products.jsx
+++ src/contexts/products.jsx
@@ -1,12 +1,13 @@
 import { createContext, useState, useEffect } from "react";
-import PRODUCTS from "../shop-data.json";
+
+import { addCollectionAndDocuments } from "../utils/firebase/firebase.jsx";
 
 export const ProductsContext = createContext({
   products: [],
 });
 
 export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState(PRODUCTS);
+  const [products, setProducts] = useState([]);
 
   const value = { products };
 
--- src/utils/firebase/firebase.jsx
+++ src/utils/firebase/firebase.jsx
@@ -9,7 +9,14 @@ import {
   signOut,
   onAuthStateChanged,
 } from "firebase/auth";
-import { getFirestore, doc, getDoc, setDoc } from "firebase/firestore";
+import {
+  getFirestore,
+  doc,
+  getDoc,
+  setDoc,
+  collection,
+  writeBatch,
+} from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD2elNjSSRD-eXxsDHQbXCvvmuIfAUCNwE",
@@ -34,6 +41,23 @@ export const signInWithGooglePopup = () =>
   signInWithPopup(auth, googleProvider);
 
 export const db = getFirestore();
+
+export const addCollectionAndDocuments = async (
+  collectionKey,
+  objectsToAdd
+) => {
+  const collectionRef = collection(db, collectionKey);
+
+  const batch = writeBatch(db);
+
+  objectsToAdd.forEach((object) => {
+    const docRef = doc(collectionRef, object.title.toLowerCase());
+    batch.set(docRef, object);
+  });
+
+  await batch.commit();
+  console.log("Done");
+};
 
 export const createUserDocument = async (
   userAuth,
